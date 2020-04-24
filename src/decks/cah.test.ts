0492b672701b1eed289600138dadbac6283393cf
Joe Mainwaring <joseph.mainwaring@gmail.com>
2020-04-24 12:56:44 -0500

    better tests

--- src/decks/cah.test.ts
+++ src/decks/cah.test.ts
@@ -1,8 +1,20 @@
 import { blackDeck, whiteDeck } from "./cah";
 
 describe("Cards Against Humanity: Black Deck", () => {
-  it("export exists", () => expect(blackDeck).not.toBeUndefined());
+  it("Export exists", () => expect(blackDeck).not.toBeUndefined());
+  it("Each card contains properties 'color', 'text' and 'pick'", () => {
+    const [card] = blackDeck;
+    expect(card).toHaveProperty("color");
+    expect(card).toHaveProperty("text");
+    expect(card).toHaveProperty("pick");
+  });
 });
+
 describe("Cards Against Humanity: White Deck", () => {
-  it("export exists", () => expect(whiteDeck).not.toBeUndefined());
+  it("Export exists", () => expect(whiteDeck).not.toBeUndefined());
+  it("Each card contains properties 'text' and 'color'", () => {
+    const [card] = whiteDeck;
+    expect(card).toHaveProperty("color");
+    expect(card).toHaveProperty("text");
+  });
 });
